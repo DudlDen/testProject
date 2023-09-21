64effa91ca59a242d55777d4629982ea8fd558a9
dennmans23 <dennmans23@gmail.com>
2023-09-21 13:44:10 +0500

    add AsyncStorage

--- App.tsx
+++ App.tsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAppState } from "./src/hooks/useAppState";
 import { AppContext } from "./src/context/AppContext";
 import { TodoList } from "./src/components/TodoList";
-import { Keyboard, Pressable } from "react-native";
+import { ActivityIndicator, Keyboard, Pressable, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
 function App() {
   const appState = useAppState();
+  const {
+    setActiveItems,
+    setActiveTab,
+    setTodoItems
+  } = appState;
+  const [loading, setLoading] = useState<boolean>(true);
+  
+  useEffect(() => {
+    AsyncStorage.getItem("appState").then(res => {
+      if (res) {
+        setActiveTab(JSON.parse(res).activeTab);
+        setActiveItems(JSON.parse(res).activeItems);
+        setTodoItems(JSON.parse(res).todoItems);
+      }
+    }).finally(() => {
+      setLoading(false);
+    });
+  }, []);
+  
+  if (loading)
+    return (
+      <View style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+      }}>
+        <ActivityIndicator />
+      </View>
+    );
   
   return (
     <Pressable style={{ flex: 1 }} onPress={() => Keyboard.dismiss()}>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -5,16 +5,18 @@ import { AppContext } from "../context/AppContext";
 import { Input } from "./Input";
 import { ButtonPanel } from "./ButtonPanel";
 import { HeaderPanel } from "./HeaderPanel";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 type Props = {};
 
 export function TodoList({}: Props) {
+  const appState = useContext(AppContext);
   const {
     activeItems,
     todoItems,
     setActiveItems,
     activeTab
-  } = useContext(AppContext);
+  } = appState;
   
   useEffect(() => {
     switch (activeTab) {
@@ -28,23 +30,19 @@ export function TodoList({}: Props) {
         setActiveItems(todoItems.filter(item => item.completed));
         break;
     }
-    
   }, [todoItems]);
   
   useEffect(() => {
-    switch (activeTab) {
-      case "All":
-        setActiveItems(todoItems);
-        break;
-      case "Active":
-        setActiveItems(todoItems.filter(item => !item.completed));
-        break;
-      case "Completed":
-        setActiveItems(todoItems.filter(item => item.completed));
-        break;
-    }
-    
-  }, [todoItems]);
+    AsyncStorage.setItem("appState", JSON.stringify(appState), (err) => {
+      if (err) {
+        console.log("an error");
+        throw err;
+      }
+      console.log("success");
+    }).catch((err) => {
+      console.log("error is: " + err);
+    });
+  }, [todoItems, activeTab, activeTab, activeItems]);
   
   return (
     <KeyboardAvoidingView style={$container}>
